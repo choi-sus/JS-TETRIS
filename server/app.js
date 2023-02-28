8aabd21fe8cb166a48b4f2777cb9b0c146473c1d
choi-sus <choi-sus@naver.com>
2023-02-28 10:17:41 +0900

    [Feat] : socket.io 연결

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const http = require("http");
 const app = express();
 const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "../client")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`abc ${PORT}`));
+io.on("connection", (socket) => {
+  console.log("연결");
+});
+
+server.listen(PORT, () => console.log(`abc ${PORT}`));
