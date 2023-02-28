502081dbc0da6b50d5af3e3bb5d18f3ce3212251
choi-sus <choi-sus@naver.com>
2023-02-28 12:27:17 +0900

    [Feat] : send button disabled 처리

--- client/js/nickname.js
+++ client/js/nickname.js
@@ -3,6 +3,11 @@ const nicknameInput = document.querySelector(".nickname-input");
 const sendButton = document.querySelector(".send-button");
 
 // event handler
+nicknameInput.addEventListener(
+  "keyup",
+  (event) => (sendButton.disabled = event.target.value ? false : true)
+);
+
 sendButton.addEventListener("click", () => {
   const nickname = nicknameInput.value;
   sessionStorage.setItem("nickname", nickname);
